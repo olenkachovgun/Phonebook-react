e03287a67255326b9bccc0e416a3e8c90e2f70ee
Olena_Chovgun <olenkachovgun@gmail.com>
2025-03-02 13:17:31 +0200

    form login and registation

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Field, Form, Formik } from "formik";
+import s from "../RegistrationPage/RegistrationPage.module.css";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const handleSubmit = (values, options) => {
+    console.log(values);
+    options.resetForm();
+  };
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={s.containerForm}>
+          <label>
+            <span>Email:</span>
+            <Field name="email" />
+          </label>
+          <label>
+            <span>Password:</span>
+            <Field name="password" type="password" />
+          </label>
+          <button type="submit" className={s.btnRegister}>
+            Login
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,7 +1,41 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import s from "./RegistrationPage.module.css";
 
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  const initialValues = {
+    email: "",
+    name: "",
+    password: "",
+  };
+  const handleSubmit = (values, options) => {
+    console.log(values);
+    options.resetForm();
+  };
+  return (
+    <div>
+      <h2>Registration Page</h2>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={s.containerForm}>
+          <label>
+            <span>Name:</span>
+            <Field name="name" />
+          </label>
+          <label>
+            <span>Email:</span>
+            <Field name="email" />
+          </label>
+          <label>
+            <span>Password:</span>
+            <Field name="password" type="password" />
+          </label>
+          <button type="submit" className={s.btnRegister}>
+            Register
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default RegistrationPage;
