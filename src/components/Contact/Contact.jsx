37bea8adeee5145b2df951c6a1b5ee0a749e1963
Olena_Chovgun <olenkachovgun@gmail.com>
2025-03-06 21:35:45 +0200

    add modal for delete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,18 +6,22 @@ import { FaSquarePhone } from "react-icons/fa6";
 //import { like } from "../../redux/contactsSlice";
 import { MdDelete } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
-import { IoMdHeart } from "react-icons/io";
-import {
-  deleteContact,
-  updateLikeStatus,
-} from "../../redux/contacts/operations";
+// import { IoMdHeart } from "react-icons/io";
+import { deleteContact } from "../../redux/contacts/operations";
 import toast from "react-hot-toast";
+import { useRef } from "react";
 
 const Contact = ({ edit, name, number, id }) => {
+  const modalRef = useRef(null);
   const dispatch = useDispatch();
-  const item = useSelector((state) =>
-    state.contacts.items.find((item) => item.id === id)
-  );
+  // const item = useSelector((state) =>
+  //   state.contacts.items.find((item) => item.id === id)
+  // );
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+    toast.error(`Contact "${name}" is deleted!`);
+    modalRef.current.close();
+  };
   return (
     <li className={s.item}>
       <div>
@@ -35,13 +39,17 @@ const Contact = ({ edit, name, number, id }) => {
         </div>
       </div>
       <div className={s.contactBtn}>
-        <button
+        {/* <button
           type="button"
           onClick={() => {
             dispatch(deleteContact(id));
             toast.error(`Contact "${name}" is deleted!`);
           }}
         >
+          <MdDelete />
+          <span>Delete</span>
+        </button> */}
+        <button type="button" onClick={() => modalRef.current.showModal()}>
           <MdDelete />
           <span>Delete</span>
         </button>
@@ -58,6 +66,15 @@ const Contact = ({ edit, name, number, id }) => {
           <span>Like</span>
         </button> */}
       </div>
+      <dialog ref={modalRef} className={s.modalDel}>
+        <div className={s.modalContent}>
+          <p>Are you sure you want to delete "{name}"?</p>
+          <div className={s.modalBtn}>
+            <button onClick={handleDelete}>Delete</button>
+            <button onClick={() => modalRef.current.close()}>Cancel</button>
+          </div>
+        </div>
+      </dialog>
     </li>
   );
 };
